4029aaef5c6f057bd8dcfd5ee3e1f8ecd7acd5d1
Nurbek <aidenscoding@gmail.com>
2024-12-04 02:30:13 +0200

    fortune-cookie minor update

--- app/fortune-cookie/fortune-cookie.tsx
+++ app/fortune-cookie/fortune-cookie.tsx
@@ -10,7 +10,7 @@ interface FortuneCookieProps {
 export default function FortuneCookie({ getFortune }: FortuneCookieProps) {
   const [isOpen, setIsOpen] = useState(false)
   const audioRef = useRef<HTMLAudioElement>(null)
-  const [fortune, setFortune] = useState("The future is cloudy. You are the master of your destiny!")
+  const [fortune, setFortune] = useState("...")
 
   const playSound = () => {
     if (audioRef.current) {
@@ -22,25 +22,26 @@ export default function FortuneCookie({ getFortune }: FortuneCookieProps) {
   return (
     <>
       <audio ref={audioRef} src="/assets/fortune-cookie/crunch.mp3" />
-      <div className="flex flex-row items-center justify-center gap-0" 
+      <div className="flex flex-row items-center justify-center gap-0"
         onClick={async () => {
           setIsOpen(!isOpen)
           playSound()
-          setFortune(await getFortune())
+          if (isOpen) {
+            setFortune(". . .")
+          } else {
+            setFortune(await getFortune())
+          }
         }}>
-        <div id="leftCookie" className={`w-20 h-40 transition-transform duration-500 ${
-          isOpen ? '-translate-x-16 -rotate-45' : 'translate-x-0 rotate-0'
-        }`}>
+        <div id="leftCookie" className={`w-20 h-40 transition-transform duration-500 ${isOpen ? '-translate-x-16 -rotate-45' : 'translate-x-0 rotate-0'
+          }`}>
           <Image quality={100} width={100} height={100} src="/assets/fortune-cookie/left-min.png" alt="left half of chocolate cookie" />
         </div>
-        <div id="fortune" className={`max-w-64 text-center bg-white mx-4 mb-4 shadow-md rounded-md max-h-content p-4 transition-all duration-500 ${
-          isOpen ? 'visible scale-100' : 'invisible scale-0 hidden'
-        }`}>
+        <div id="fortune" className={`max-w-64 text-center bg-white mx-4 mb-4 shadow-md rounded-md max-h-content p-4 transition-all duration-500 ${isOpen ? 'visible scale-100' : 'invisible scale-0 hidden'
+          }`}>
           <p className='font-serif text-zinc-700 text-pretty text-sm'>{fortune}</p>
         </div>
-        <div id='rightCookie' className={`w-20 h-40 transition-transform duration-500 ${
-          isOpen ? 'translate-x-16 rotate-45' : 'translate-x-0 rotate-0'
-        }`}>
+        <div id='rightCookie' className={`w-20 h-40 transition-transform duration-500 ${isOpen ? 'translate-x-16 rotate-45' : 'translate-x-0 rotate-0'
+          }`}>
           <Image quality={100} width={100} height={100} src="/assets/fortune-cookie/right-min.png" alt="right half of chocolate cookie" />
         </div>
       </div>
