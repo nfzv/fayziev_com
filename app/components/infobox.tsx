0123b8ef6b957f26f58d35f17655128c3d62c514
Nurbek <aidenscoding@gmail.com>
2025-08-09 15:36:02 +0300

    ui corrections

--- app/components/infobox.tsx
+++ app/components/infobox.tsx
@@ -4,7 +4,7 @@ export interface InfoBoxProps {
 
 export function InfoBox({ message }: InfoBoxProps) {
   return (
-    <div className="bg-yellow-100 border-l-4 border-black-200 p-2">
+    <div className="bg-yellow-100 border-l-4 border-black-200 p-2 mt-2 mb-2">
       <div className="flex">
         <div className="ml-3">
           <p className="text-md text-blue-700">
@@ -21,7 +21,7 @@ export function InfoBox({ message }: InfoBoxProps) {
 
 export function InfoBoxAny({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-yellow-100 border-l-4 border-black-200 p-2">
+    <div className="bg-yellow-100 border-l-4 border-black-200 p-2 mt-2 mb-2">
       <div className="flex">
         <div className="ml-3">
           <div className="text-md text-blue-700">
