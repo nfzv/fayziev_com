3c0dd39f4865e8d64755fd5bff827d00917af997
Nurbek <51998193+nfzv@users.noreply.github.com>
2026-03-04 00:12:51 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from './components/footer'
 import { baseUrl } from './sitemap'
 import { ViewCountsProvider } from './contexts/view-counts-context'
+import Script from "next/script"
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
@@ -52,7 +53,12 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-    >
+    ><head>
+        <Script data-goatcounter="https://nurbek.goatcounter.com/count"
+          src="//gc.zgo.at/count.js"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className="antialiased max-w-2xl mx-4 mt-4 lg:mx-auto">
         <main className="flex-auto min-w-0 flex flex-col px-2 md:px-0">
           <ViewCountsProvider>
